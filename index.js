08714ecbeeadc0eadbde92881211db1f48fa1be8
CaiooSouzaa <caioggsouza@hotmail.com>
2025-05-13 23:59:20 -0300

    Jogo escobra p1

--- index.js
+++ index.js
@@ -1,42 +1,58 @@
 const canvas = document.querySelector("canvas")
+const score = document.querySelector(".score--value")
+const finalScore = document.querySelector(".final-score > span")
+const menu = document.querySelector(".menu-screen")
+const buttonPlay = document.querySelector(".btn-play")
+
 const ctx = canvas.getContext("2d")
-let h1 =document.querySelector("h1")
+
+let h1 = document.querySelector("h1")
+
+let direcao, loopId
 
 const size = 30
 const cobra = [
-    { x: 270, y: 240 }
+    { x: 270, y: 240 },
+    { x: 300, y: 240 },
+    { x: 330, y: 240 }  
 ]
 
-const randomNumber = (min, max) =>{
+//INCREMENTA PONTOS
+const incrementScore = () =>{
+    score.innerText = parseInt(score.innerText) + 10
+}
+
+const randomNumber = (min, max) => {
     return Math.round(Math.random() * (max - min) + min)
 }
 
-const randomPosition = () =>{
+const randomPosition = () => {
     const number = randomNumber(0, canvas.width - size)
-    return Math.round(number/30) * 30
-}  
+    return Math.round(number / 30) * 30
+}
 
 //FUNÇÃO PARA GERAR CORES ALEATORIAS
-const randomColor = () =>{
-    const red = randomNumber(0 ,255)
+const randomColor = () => {
+    const red = randomNumber(0, 255)
     const green = randomNumber(0, 255)
     const blue = randomNumber(0, 255)
 
     return `rgb(${red}, ${green}, ${blue})`
 }
 
+//CONFIGURAÇÃO PARA GERA PONTOS ALEATORIOS E DE CORES ALEATORIAS
 const pontos = {
-    x: randomPosition(), 
-    y:randomPosition(), 
+    x: randomPosition(),
+    y: randomPosition(),
     color: randomColor()
 }
 
-let direcao, loopId
+
 
 //FUNÇÃO QUE CRIA COMIDA 
-const drawPontos = () =>{
+const drawPontos = () => {
 
-    const {x, y, color} = pontos
+    const { x, y, color } = pontos
 
     ctx.shadowColor = color
     ctx.shadowBlur = 50
@@ -98,8 +114,50 @@ const drawGrid = () => {
 }
 
 //CHECAR SE COMEU O PONTO
-const checkEat = () =>{
-    
+const checkEat = () => {
+    const cabeca = cobra[cobra.length - 1]
+
+    if (cabeca.x == pontos.x && cabeca.y == pontos.y) {
+        incrementScore()
+        cobra.push(cabeca)
+        let x = randomPosition()
+        let y = randomPosition()
+
+        while (cobra.find((posicao) => posicao.x == x && posicao.y == y)) {
+            x = randomPosition()
+            y = randomPosition()
+        }
+
+        pontos.x = x
+        pontos.y = y
+        pontos.color = randomColor()
+    }
+}
+
+//FUNÇÃO DE COLISSÕES
+const checkCollision = () =>{
+    const cabeca = cobra[cobra.length - 1]
+
+    const canvasLimit = canvas.width - size
+
+    const neckIndex = cobra.length - 1
+
+    const wallCollision = cabeca.x < 0 || cabeca.x > 507 || cabeca.y < 0 || cabeca.y > 570
+
+    const selfCollision = cobra.find((posicao, index) =>{
+        return index < neckIndex && posicao.x == cabeca.x && posicao.y == cabeca.y
+    })
+
+    if(wallCollision || selfCollision){
+        alert('Game Over')
+    }
+}
+
+//FUNÇÃO DE GAME OVER
+const gameOver = () =>{
+    direcao = undefined
+    menu.style.display = "flex"
+    finalScore.innerText - score 
 }
 
 //LOOP DA COBRA 
@@ -109,8 +167,11 @@ const gameLoop = () => {
     ctx.clearRect(0, 0, 600, 600)
     drawGrid()
     drawPontos()
-    drawSnake()
     moveSnake()
+    drawSnake()
+    checkEat()
+    checkCollision()
+    
 
     loopId = setTimeout(() => {
         gameLoop()
@@ -120,8 +181,6 @@ const gameLoop = () => {
 }
 
 
-
-h1.innerText = randomColor()
 gameLoop()
 
 //AÇÃO DE CADA BOTÃO AO SER APERTADO
